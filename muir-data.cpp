4e18655471a4f241a628745bffc9cba020d4baf6
Beau V.C. Bellamy <bvbellamy@alaska.edu>
2010-10-14 17:18:59 -0800

    - Added axis labels

--- muir-data.cpp
+++ muir-data.cpp
@@ -15,6 +15,7 @@
 #include "muir-data.h"
 
 #include <gd.h>
+#include <gdfontl.h>
 #include "H5Cpp.h"
 
 #include <iostream>  // std::cout
@@ -546,15 +547,17 @@ void MuirData::save_2dplot(const std::string &output_file)
     std::size_t dataset_count  = 10;   // # sets
     std::size_t dataset_height = 1100; // Range bins
 
-    std::size_t axis_x_height = 10;
+    std::size_t axis_x_height = 40;
+    std::size_t axis_y_width  = 40;
     std::size_t border = 1;
+    std::size_t colorbar_width = 60;
 
     std::size_t start_frame = (*_framecount)[0][0];
     std::size_t end_frame = (*_framecount)[9][499];
     std::size_t num_frames = end_frame - start_frame;
 
-    std::size_t width            = (num_frames)/delta_t+(border*4)+20;
-    std::size_t height           = dataset_height+(2*border)+axis_x_height; 
+    std::size_t width            = (num_frames)/delta_t+(border*4)+ colorbar_width + axis_y_width;
+    std::size_t height           = dataset_height + (2*border) + axis_x_height; 
     int         bit_depth        = 8;
 
     // Open File for Writing
@@ -622,13 +625,13 @@ void MuirData::save_2dplot(const std::string &output_file)
                 if (delta_t == 1)
                 {
                     unsigned char pixel = static_cast<unsigned int>(std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][k][i][0], (*_sample_data)[set][k][i][1])))*10*4));
-                    gdImageSetPixel(im, border + frameoffset + k, dataset_height-i, pixel);
+                    gdImageSetPixel(im, (axis_y_width + border) + frameoffset + k, dataset_height-i, pixel);
                 }
                 else if (delta_t == 2)
                 {
                     float col1 = std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][delta_t*k][i][0], (*_sample_data)[set][delta_t*k][i][1])))*10*4);
                     float col2 = std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][delta_t*k+1][i][0], (*_sample_data)[set][delta_t*k+1][i][1])))*10*4);
-                    gdImageSetPixel(im, border + frameoffset + k, dataset_height-i, static_cast<unsigned char>((col1 + col2)/2.0));
+                    gdImageSetPixel(im, (axis_y_width + border) + frameoffset + k, dataset_height-i, static_cast<unsigned char>((col1 + col2)/2.0));
                 }
                 else if (delta_t == 4) // It might be better if this was a loop.....  just a thought.  It does look rather loopworthy...
                 {
@@ -636,14 +639,14 @@ void MuirData::save_2dplot(const std::string &output_file)
                     float col2 = std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][delta_t*k+1][i][0], (*_sample_data)[set][delta_t*k+1][i][1])))*10*4);
                     float col3 = std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][delta_t*k+2][i][0], (*_sample_data)[set][delta_t*k+2][i][1])))*10*4);
                     float col4 = std::min(254.0,log10(norm(std::complex<float>((*_sample_data)[set][delta_t*k+3][i][0], (*_sample_data)[set][delta_t*k+3][i][1])))*10*4);
-                    gdImageSetPixel(im, border + frameoffset + k, dataset_height-i, static_cast<unsigned char>((col1 + col2 + col3 + col4)/4.0));
+                    gdImageSetPixel(im, (axis_y_width + border) + frameoffset + k, dataset_height-i, static_cast<unsigned char>((col1 + col2 + col3 + col4)/4.0));
                 }
             }
         }
 
         // Color bar
-        for (std::size_t col = 0; col < 20; col++)
-            gdImageSetPixel(im, width-col-2, dataset_height-i, static_cast<unsigned char>((float(i)/dataset_height)*(256.0)));
+        for (std::size_t col = 0; col < colorbar_width/3; col++)
+            gdImageSetPixel(im, width-1-colorbar_width+col, dataset_height-i, static_cast<unsigned char>((float(i)/dataset_height)*(256.0)));
 
 
         if (!(i%10))
@@ -652,10 +655,69 @@ void MuirData::save_2dplot(const std::string &output_file)
 
     }
 
+    // Fontness
+    gdFontPtr font = gdFontGetLarge();
+    //char *str = "TEST TEST TEST TEST TEST TEST";
+    //gdImageString(im, font, 10,10, reinterpret_cast<unsigned char*>(str), white);
+    
     // Bottom Axis
-    for (std::size_t x = border; x<(width-border*2-20); x = x + 5)
-        gdImageLine(im, x, (dataset_height+border*2), x, (dataset_height + ((x-border)%10?(axis_x_height/2):axis_x_height)), white); 
 
+    std::size_t xaxis_offset = (axis_y_width + border);
+    std::size_t xaxis_end = num_frames/delta_t;
+    std::size_t xaxis_height1 = axis_x_height / 4;
+    std::size_t xaxis_height2 = axis_x_height / 8;
+    std::size_t xaxis_yoffset = dataset_height+border*2;
+
+    gdImageFilledRectangle(im, 0, xaxis_yoffset, width, height, white);
+    gdImageFilledRectangle(im, 0, 0, axis_y_width-border, height, white);
+    for (std::size_t x = 0; x < xaxis_end; x = x + 5)
+    {
+        gdImageLine(im, xaxis_offset + x, xaxis_yoffset, xaxis_offset + x, (xaxis_yoffset + ((x%10)?(xaxis_height2):xaxis_height1)), black); 
+    }
+    for (std::size_t set = 0; set < dataset_count; set++)
+    {
+        std::size_t frameoffset = ((*_framecount)[set][0]-start_frame)/delta_t + axis_y_width + border;
+        gdImageLine(im, frameoffset, xaxis_yoffset, frameoffset, xaxis_yoffset + xaxis_height1*2 , black);
+        
+        char buf1[80];
+        char buf2[80];
+        time_t then;
+        struct tm *ts;
+        then = _time[set][0]/1000000;
+        ts = gmtime(&then);
+        //strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", ts);
+        strftime(buf1, sizeof(buf1), "%H:%M:%S", ts);
+        sprintf(buf2,"%s:%06.3f",buf1, (_time[set][0]/1000000-then)*1000);
+        gdImageString(im, font, frameoffset + 2, xaxis_yoffset + xaxis_height1 + 2, reinterpret_cast<unsigned char*>(buf2), black);
+        //gdImageString(im, font, frameoffset + 2, xaxis_yoffset + xaxis_height1 + 2, reinterpret_cast<unsigned char*>(buf), black);
+    }
+
+
+    // Print date
+    {
+    char gmtbuf[80];
+    char lclbuf[80];
+    char buf2[200];
+    time_t then;
+    struct tm *ts;
+    then = _time[0][0]/1000000;
+    ts = gmtime(&then);
+    strftime(gmtbuf, sizeof(gmtbuf), "%a %Y-%m-%d %H:%M:%S %Z", ts);
+    ts = localtime(&then);
+    strftime(lclbuf, sizeof(lclbuf), "%a %Y-%m-%d %H:%M:%S %Z", ts);
+    sprintf(buf2,"Experiment Date: %s (%s)   File: %s   Column Width: %d Frame(s)", gmtbuf, lclbuf,  _filename.c_str(), delta_t);
+    gdImageString(im, font, axis_y_width + border, xaxis_yoffset + xaxis_height1*2 + 5, reinterpret_cast<unsigned char*>(buf2), black);
+    }
+
+    // Left Axis
+    for (std::size_t y = 50; y <= dataset_height; y = y + 80)
+    {
+        gdImageLine(im, axis_y_width/2, y, axis_y_width, y, black);
+        char buf[80];
+        sprintf(buf,"%-3.1fkm",((*_sample_range)[0][1100-y])/1000);
+        gdImageStringUp(im, font, 5, y+border+20, reinterpret_cast<unsigned char*>(buf), black);
+        //std::cout << y << ":" << 1099-y << "-" << (*_sample_range)[0][1099-y]/1000 << " " << std::endl;
+    }
 
     // Done with file
     std::cerr << "WRITING IMAGE " << std::endl;
