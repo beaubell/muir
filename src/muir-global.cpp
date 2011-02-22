cdc532c568ca8006612d90fd94dc4db91f52ce46
Beau V.C. Bellamy <bvbellamy@alaska.edu>
2011-02-22 11:33:57 -0900

    - Add a definition for common global variables. (like verbosity)

---
+++ src/muir-global.cpp
@@ -0,0 +1,15 @@
+//
+// C++ Implementation: muir-global
+//
+// Description: Global state initialization
+//
+//
+// Author: Beau V.C. Bellamy <bvbellamy@arsc.edu>
+//         Arctic Region Supercomputing Center
+//
+//
+//
+
+#include "muir-global.h"
+
+bool MUIR_Verbose = false;
---
+++ src/muir-global.h
@@ -0,0 +1,17 @@
+#ifndef MUIR_GLOBAL_H
+#define MUIR_GLOBAL_H
+//
+// C++ Interface: muir-global
+//
+// Description: Here is where global state lies.
+//
+//
+// Author: Beau V.C. Bellamy <bvbellamy@arsc.edu>
+//         Arctic Region Supercomputing Center
+//
+//
+//
+
+extern bool MUIR_Verbose;
+
+#endif //MUIR_GLOBAL_H
